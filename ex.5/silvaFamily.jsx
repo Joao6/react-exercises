d76351c7998045828d39921555edb63df0af4571
Joao Pedro <joaopedro@fai-mg.br>
2018-07-23 17:31:41 -0300

    Added fifth exercise

---
+++ ex.5/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Member from "./member";
+
+export default () => (
+  <div>
+    <Member name="Joao" lastName="Silva" />
+    <Member name="Maria" lastName="Silva" />
+    <Member name="Amadeu" lastName="Silva" />
+    <Member name="Luciana" lastName="Silva" />
+  </div>
+);
